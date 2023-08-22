a54dc9ea96be6241745e88dd75b5cd820bbab595
marcelogmoura <mgmoura@gmail.com>
2023-08-21 22:03:11 -0300

    feat: menu e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ProdutoCadastroComponent } from './components/produto-cadastro/produto-cadastro.component';
 import { ProdutoConsultaComponent } from './components/produto-consulta/produto-consulta.component';
 import { MovimentoCadastroComponent } from './components/movimento-cadastro/movimento-cadastro.component';
 import { MovimentoConsultaComponent } from './components/movimento-consulta/movimento-consulta.component';
+
+const routes: Routes = [
+
+  { path: 'produto-cadastro' , component: ProdutoCadastroComponent },
+  { path: 'produto-consulta', component: ProdutoConsultaComponent },
+  { path: 'movimento-cadastro', component: MovimentoCadastroComponent },
+  { path: 'movimento-consulta' , component: MovimentoConsultaComponent}
+
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +27,8 @@ import { MovimentoConsultaComponent } from './components/movimento-consulta/movi
     MovimentoConsultaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
